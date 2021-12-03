bd77c67f8cbf09518574aa1d28b45a04df71991c
Vic Chimenti <victor.chimenti@gmail.com>
2021-12-03 11:21:41 -0800

    rm cm cd assignlist

--- law-text-html.js
+++ law-text-html.js
@@ -50,28 +50,10 @@
          }
      }
  }
+
  
  
- 
- 
- /***
-  *      Returns an array of list items
-  */
-//  function assignList(arrayOfValues) {
- 
-//      let listValues = '';
- 
-//      for (let i = 0; i < arrayOfValues.length; i++) {
- 
-//          listValues += '<li class="tag">' + arrayOfValues[i].trim() + '</li>';
-//      }
- 
-//      return listValues;
-//  }
- 
- 
- 
- 
+  
  /***
   *      Write the document
   */
